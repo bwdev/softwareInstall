e19a5cb5da2616024dc949b24f5458827974b414
bmeek <bmeek@semgroupcorp.com>
2019-01-21 13:07:09 -0600

    add vscode extensions script

---
+++ installVSCodeExtensions.bat
@@ -0,0 +1,36 @@
+echo Installing VSCode Extensions...
+call code --install-extension abhiagr.livs --force
+call code --install-extension bierner.lit-html --force
+call code --install-extension CoenraadS.bracket-pair-colorizer --force
+call code --install-extension cstechnologies.vscode-mocha-test-runner --force
+call code --install-extension dbaeumer.vscode-eslint --force
+call code --install-extension dbankier.vscode-instant-markdown --force
+call code --install-extension donjayamanne.githistory --force
+call code --install-extension DSKWRK.vscode-generate-getter-setter --force
+call code --install-extension eamodio.gitlens --force
+call code --install-extension eg2.tslint --force
+call code --install-extension filipw.scriptcsRunner --force
+call code --install-extension firsttris.vscode-jest-runner --force
+call code --install-extension Gruntfuggly.todo-tree --force
+call code --install-extension HookyQR.beautify --force
+call code --install-extension humao.rest-client --force
+call code --install-extension jinmingjian.sde --force
+call code --install-extension McCarter.start-git-bash --force
+call code --install-extension ms-mssql.mssql --force
+call code --install-extension ms-vscode.csharp --force
+call code --install-extension ms-vscode.PowerShell --force
+call code --install-extension msjsdiag.debugger-for-chrome --force
+call code --install-extension Orta.vscode-jest --force
+call code --install-extension pranaygp.vscode-css-peek --force
+call code --install-extension quicktype.quicktype --force
+call code --install-extension robertohuertasm.vscode-icons --force
+call code --install-extension Shan.code-settings-sync --force
+call code --install-extension SirTori.indenticator --force
+call code --install-extension sleistner.vscode-fileutils --force
+call code --install-extension vincaslt.highlight-matching-tag --force
+call code --install-extension WallabyJs.quokka-vscode --force
+call code --install-extension wayou.vscode-todo-highlight --force
+call code --install-extension wix.vscode-import-cost --force
+call code --install-extension xabikos.JasmineSnippets --force
+call code --install-extension Zignd.html-css-class-completion --force
+echo Done Installing Extensions...
